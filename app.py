4a8fbc2a212d2c5765a6ac945d9412f1cb617c41
justchokingaround <ivanonarch@tutanota.com>
2025-07-05 17:35:20 +0200

    fix: proper YAML file processing for adjacency matrix generation

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import sys
 import os
 import yaml
-from adjacency_matrix import AdjacencyMatrix
+from werkzeug.utils import secure_filename
+from adjacency_matrix import AdjacencyMatrix, parse_yaml_to_adjacency_matrix
 
 
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
@@ -11,6 +12,8 @@ from traces_to_matrix import traces_to_adjacency_matrix
 from dependencies import TemporalType, ExistentialType, Direction, TemporalDependency, ExistentialDependency
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'temp_uploads'
+os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
 
 current_matrix = None
 
@@ -26,55 +29,14 @@ def process_input():
     try:
         if 'file' in request.files and request.files['file'].filename != '':
             file = request.files['file']
-            if file.filename.endswith('.yaml') or file.filename.endswith('.yml'):
-                yaml_data = yaml.safe_load(file.stream)
+            if file and (file.filename.endswith('.yaml') or file.filename.endswith('.yml')):
+                filename = secure_filename(file.filename)
+                filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+                file.save(filepath)
                 
-                metadata = yaml_data.get('metadata', {})
-                activities = metadata.get('activities', [])
-                dependencies = yaml_data.get('dependencies', [])
-
-                current_matrix = AdjacencyMatrix(activities=activities)
-
-                for dep in dependencies:
-                    from_activity = dep.get('from')
-                    to_activity = dep.get('to')
-                    
-                    if not from_activity or not to_activity:
-                        continue
-                    
-                    temporal_dep = None
-                    if 'temporal' in dep:
-                        temporal_data = dep['temporal']
-                        dep_type_str = temporal_data.get('type', '').upper()
-                        direction_str = temporal_data.get('direction', '').upper()
-                        
-                        try:
-                            dep_type = TemporalType[dep_type_str] if dep_type_str else None
-                            direction = Direction[direction_str] if direction_str else None
-
-                            if dep_type and direction:
-                                temporal_dep = TemporalDependency(dep_type, direction)
-                        except KeyError:
-                            pass # Ignore if type or direction is invalid
-
-                    existential_dep = None
-                    if 'existential' in dep:
-                        existential_data = dep['existential']
-                        dep_type_str = existential_data.get('type', '').upper()
-                        direction_str = existential_data.get('direction', '').upper()
-                        
-                        try:
-                            dep_type = ExistentialType[dep_type_str] if dep_type_str else None
-                            direction = Direction[direction_str] if direction_str else None
-
-                            if dep_type and direction:
-                                existential_dep = ExistentialDependency(dep_type, direction)
-                        except KeyError:
-                            pass # Ignore if type or direction is invalid
-                    
-                    if temporal_dep or existential_dep:
-                        current_matrix.add_dependency(from_activity, to_activity, temporal_dep, existential_dep)
-
+                current_matrix = parse_yaml_to_adjacency_matrix(filepath)
+                
+                os.remove(filepath) # Clean up the temporary file
             else:
                 return jsonify({"success": False, "error": "Invalid file type. Please upload a YAML file."})
 
@@ -111,23 +73,30 @@ def get_matrix():
             if dep_tuple:
                 temporal_dep, existential_dep = dep_tuple
                 
+                is_temporal_independence = False
                 temporal_str = "-"
                 if temporal_dep:
-                    direction_symbol = ""
-                    if temporal_dep.direction == Direction.FORWARD:
-                        direction_symbol = "≺"
-                    elif temporal_dep.direction == Direction.BACKWARD:
-                        direction_symbol = "≻"
-                    
-                    type_symbol = ""
-                    if temporal_dep.type == TemporalType.DIRECT:
-                        type_symbol = "d"
-                    
-                    temporal_str = f"{direction_symbol}{type_symbol}"
+                    if temporal_dep.type == TemporalType.INDEPENDENCE:
+                        is_temporal_independence = True
+                    else:
+                        direction_symbol = "-"
+                        if temporal_dep.direction == Direction.FORWARD:
+                            direction_symbol = "≺"
+                        elif temporal_dep.direction == Direction.BACKWARD:
+                            direction_symbol = "≻"
+                        
+                        type_symbol = ""
+                        if temporal_dep.type == TemporalType.DIRECT:
+                            type_symbol = "d"
+                        
+                        temporal_str = f"{direction_symbol}{type_symbol}"
 
+                is_existential_independence = False
                 existential_str = "-"
                 if existential_dep:
-                    if existential_dep.type == ExistentialType.IMPLICATION:
+                    if existential_dep.type == ExistentialType.INDEPENDENCE:
+                        is_existential_independence = True
+                    elif existential_dep.type == ExistentialType.IMPLICATION:
                         if existential_dep.direction == Direction.FORWARD:
                             existential_str = "=>"
                         elif existential_dep.direction == Direction.BACKWARD:
@@ -141,7 +110,9 @@ def get_matrix():
                     elif existential_dep.type == ExistentialType.OR:
                         existential_str = "∨"
 
-                if temporal_str != "-" or existential_str != "-":
+                if is_temporal_independence and is_existential_independence:
+                    matrix_display[from_activity][to_activity] = "-,-"
+                elif temporal_str != "-" or existential_str != "-":
                     matrix_display[from_activity][to_activity] = f"{temporal_str},{existential_str}"
                 else:
                     matrix_display[from_activity][to_activity] = ""
